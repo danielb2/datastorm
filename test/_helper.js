171d331dd578267f73ea72a1466b850502c53db3
Daniel Bretoi <daniel@bretoi.com>
2015-01-26 19:54:02 -0800

    convert to javascript

--- test/_helper.js
+++ test/_helper.js
@@ -1,6 +1,5 @@
 process.env.NODE_ENV = 'test';
 
-require('coffee-script');
 DataStorm = require('../lib/datastorm');
 require('chai').should();
 // chai.Assertion.includeStack = true; // defaults to false
