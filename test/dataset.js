9e6af12a9e1a5d4f8f851347efde76a109ababd8
Daniel Bretoi <daniel@bretoi.com>
2015-02-11 15:53:17 -0800

    add ability to do raw sql

--- test/dataset.js
+++ test/dataset.js
@@ -65,6 +65,14 @@ describe("Dataset", function() {
     });
 
 
+    it("should do raw sql", function (done) {
+
+        dataset.limit(3).raw('SeLeCt id from `items`').order('id', 'asc').sql().should.equal("SeLeCt id from `items` ORDER BY `id` LIMIT 3");
+        dataset.raw('SeLeCt id from `items`').raw('LIMIT 4').sql().should.equal("SeLeCt id from `items` LIMIT 4");
+        done();
+    });
+
+
     it("should limit", function (done) {
 
         dataset.limit(3).sql().should.equal("SELECT * FROM `items` LIMIT 3");
